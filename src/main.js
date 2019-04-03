fd73547a6f8cf7054e51a051e0b00344ea75d532
Thomas William McClean <spam@tommcclean.me>
2019-04-03 20:56:30 +0100

    Remove globally registered lodash as it is now imported into relevant functions as needed

--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import router from '@/startup/routes-manifest.js';
 import store from '@/startup/setup-store.js';
 
 import '@/startup/axios-setup.js';
-import '@/startup/plugins-setup.js';
 import '@/startup/common-components.js';
+
+import 'animate.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
 	router,
 	render: h => h(App),
-	store: store,
+	store,
 }).$mount('#app');
--- src/startup/plugins-setup.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import lodash from 'lodash'
-import 'animate.css';
-
-Vue.prototype._ = lodash
